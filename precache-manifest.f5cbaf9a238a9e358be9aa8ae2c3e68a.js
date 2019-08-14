1287379a520fd7234a602245f065de2e29da2d60
Rosa Kang <therosessom@gmail.com>
2019-08-14 16:11:15 -0700

    Updates

---
+++ precache-manifest.f5cbaf9a238a9e358be9aa8ae2c3e68a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6821903be3ae932b647819712987076d",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "e5abafcf1aba845f15ed",
+    "url": "/ecommerce/static/css/2.7ec30cc4.chunk.css"
+  },
+  {
+    "revision": "d10319f6f296b89396ed",
+    "url": "/ecommerce/static/css/main.84fe3d88.chunk.css"
+  },
+  {
+    "revision": "e5abafcf1aba845f15ed",
+    "url": "/ecommerce/static/js/2.99ef66cd.chunk.js"
+  },
+  {
+    "revision": "d10319f6f296b89396ed",
+    "url": "/ecommerce/static/js/main.3dfd9dc7.chunk.js"
+  },
+  {
+    "revision": "e176d1401fcedba87ff8",
+    "url": "/ecommerce/static/js/runtime~main.8409c7fe.js"
+  },
+  {
+    "revision": "0feb92b0924cac680e5ffaa38195851c",
+    "url": "/ecommerce/static/media/cart.0feb92b0.png"
+  },
+  {
+    "revision": "f187fe05973c61182099c73287433e19",
+    "url": "/ecommerce/static/media/cartScr.f187fe05.png"
+  },
+  {
+    "revision": "ef1875cca694e967edd5d48a1dd79f0b",
+    "url": "/ecommerce/static/media/effortless.ef1875cc.png"
+  },
+  {
+    "revision": "c2ce59b0e0c7d130b77be860120adb13",
+    "url": "/ecommerce/static/media/footage.c2ce59b0.jpg"
+  },
+  {
+    "revision": "f0d1a59d1bd329e043f78511cac4c716",
+    "url": "/ecommerce/static/media/insta.f0d1a59d.png"
+  },
+  {
+    "revision": "ed28a4b02f68700225afb1e594eb497f",
+    "url": "/ecommerce/static/media/instaScr.ed28a4b0.png"
+  },
+  {
+    "revision": "498b939d35f76551968ce4ffcca76d28",
+    "url": "/ecommerce/static/media/pintScr.498b939d.png"
+  },
+  {
+    "revision": "aa3a1e4273820c0b33ae28bf5b015a9f",
+    "url": "/ecommerce/static/media/pinterest.aa3a1e42.png"
+  },
+  {
+    "revision": "aad9aa563579556adba18aea0553a3a8",
+    "url": "/ecommerce/static/media/play.aad9aa56.png"
+  },
+  {
+    "revision": "05d08100ef32f562491145632bf1ef11",
+    "url": "/ecommerce/static/media/sc-effort.05d08100.png"
+  },
+  {
+    "revision": "62d480da096d3afdd3b007f9dccbbbd2",
+    "url": "/ecommerce/static/media/search.62d480da.png"
+  },
+  {
+    "revision": "e4fd33ea298f1b174cb4c727d4383cf1",
+    "url": "/ecommerce/static/media/searchScr.e4fd33ea.png"
+  }
+]);
